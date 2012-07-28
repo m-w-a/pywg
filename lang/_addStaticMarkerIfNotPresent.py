6a60792cbecff709bba5b65a33f661af1a6da954
Mostafa <mostafa_working_away@yahoo.com>
2012-07-27 22:26:32 -0700

    wg.lang module

---
+++ lang/_addStaticMarkerIfNotPresent.py
@@ -0,0 +1,22 @@
+import types
+
+def addStaticMarkerIfNotPresent(\
+  func : (types.FunctionType, types.MethodType), \
+  marker : str) \
+    -> bool:
+  """Adds the marker 'marker' to 'func' as an attribute, if not already
+  present. 'marker' maybe any string, including an invalid identifier. Returns 
+  whether addition was successful.
+  
+  Use this to add static variables to functions, like so:
+  
+    def foo(...):
+      if addStaticMarkerIfNotPresent(foo, '$sm'):
+        foo.StaticVar = ...
+  """
+  
+  if getattr(func, marker, func) is func:
+    setattr(func, marker, True)
+    return True
+  else:
+    return False
---
+++ lang/addStaticMarkerIfNotPresent_PyUnit.py
@@ -0,0 +1,81 @@
+from _addStaticMarkerIfNotPresent import *
+import unittest
+
+class Test_addStaticMarkerIfNotPresent(unittest.TestCase):
+  
+  def setUp(self):
+    self.__markers = []
+    self.__funcs = []
+    
+    validIdentifiers = ['validId', '_St', '__my_id', 'camelCase_w']
+    invalidIdentifiers = ['$id', 'id with spaces  ', '$9808 $#!(*%&']
+    
+    self.__markers.extend(validIdentifiers)
+    self.__markers.extend(invalidIdentifiers)
+    
+    def emptyFunc(): pass
+    def noParamFunc(): return x;
+    def oneParamFunc(name : str) -> None: pass
+    def twoParamFunc(arg1 : int, arg2 : dict) -> float: pass
+    
+    self.__funcs.extend(\
+      [emptyFunc, noParamFunc, oneParamFunc, twoParamFunc])
+    
+    class Methods:
+      def emptyFunc(): pass
+      def noParamFunc(): return x;
+      def oneParamFunc(name : str) -> None: pass
+      def twoParamFunc(arg1 : int, arg2 : dict) -> float: pass
+
+    self.__funcs.extend([\
+        Methods.emptyFunc, \
+        Methods.noParamFunc, \
+        Methods.oneParamFunc, \
+        Methods.twoParamFunc\
+      ])
+  
+  def test_markerAdded(self):
+    
+    def test(\
+      func : (types.FunctionType, types.MethodType), \
+      marker : str) \
+        -> bool:
+      
+      didAdd = addStaticMarkerIfNotPresent(func, marker)
+      self.assertEqual(True, didAdd)
+      self.assertTrue(hasattr(func, marker))
+
+    for marker in self.__markers:
+      for func in self.__funcs:
+        test(func, marker)
+  
+  def test_markerNotAdded(self):
+
+    def addMarker(\
+      func : (types.FunctionType, types.MethodType), \
+      marker : str) \
+        -> bool:
+      
+      didAdd = addStaticMarkerIfNotPresent(func, marker)
+      self.assertEqual(True, didAdd)
+      self.assertTrue(hasattr(func, marker))
+    
+    def test(\
+      func : (types.FunctionType, types.MethodType), \
+      marker : str) \
+        -> bool:
+      
+      didAdd = addStaticMarkerIfNotPresent(func, marker)
+      self.assertEqual(False, didAdd)
+      self.assertTrue(hasattr(func, marker))
+    
+    for marker in self.__markers:
+      for func in self.__funcs:
+        addMarker(func, marker)
+    
+    for marker in self.__markers:
+      for func in self.__funcs:
+        test(func, marker)
+
+if __name__ == '__main__':
+  unittest.main()
