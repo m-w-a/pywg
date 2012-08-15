35150ef97d9db6507e1c1fb90f149754cc5b0967
Mostafa <mostafa_working_away@yahoo.com>
2012-08-15 06:48:58 -0700

    Fix so that unittest will handle any exceptions that are thrown.

--- util/tests_ExecutingScript/initNoThrow_PyUnit.py
+++ util/tests_ExecutingScript/initNoThrow_PyUnit.py
@@ -1,15 +1,22 @@
 #! /usr/bin/env python3.2
 
-def scriptBroadcaster(scriptListner):
-    scriptListner()
+_Exception = None
+try:
+    def scriptBroadcaster(scriptListner):
+        scriptListner()
 
-from ExecutingScript import *
-ExecutingScript.init('initNoThrow_PyUnit')
+    from ExecutingScript import *
+    ExecutingScript.init('initNoThrow_PyUnit')
+
+except Exception as ex:
+    _Exception = ex
 
 import unittest
 
 class Test_initNoThrow(unittest.TestCase):
-    pass
+    def test(self):
+        if _Exception is not None:
+            raise _Exception
 
 if __name__ == '__main__':
     unittest.main()
