fec4b2a8054df11e0b4f56794d6be49d882e3448
Mostafa <mostafa_working_away@yahoo.com>
2012-08-17 12:51:30 -0700

    Add new test.

---
+++ util/tests_ExecutingScript/initThrowDueToMissingScriptBroadcaster_PyUnit.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3.2
+
+_Exception = None
+try:
+
+    from ExecutingScript import *
+    ExecutingScript.init('initNoThrowDueToMissingScriptBroadcaster')
+
+except Exception as ex:
+    _Exception = ex
+
+import unittest
+
+class Test_initThrowDueToMissingScriptBroadcaster(unittest.TestCase):
+    def test(self):
+        expectedErrMsgSubStr = \
+          '__main__ module missing the following attributes: scriptBroadcaster'
+        with self.assertRaisesRegex(
+          ExecutingScript.RequirementUnsatisfiedError,
+          expectedErrMsgSubStr):
+            if(_Exception is not None):
+                raise _Exception
+
+if __name__ == '__main__':
+    unittest.main()
