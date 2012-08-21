845b8d2e9ee291b32bae0ac713cac6b19820e4d1
Mostafa <mostafa_working_away@yahoo.com>
2012-08-20 17:56:41 -0700

    Corrections.

--- util/tests_ExecutingScript/initNoThrow_PyUnit.py
+++ util/tests_ExecutingScript/initNoThrow_PyUnit.py
@@ -1,23 +1,25 @@
 #! /usr/bin/env python3.2
 
-_Exception = None
-try:
-    def scriptBroadcaster(scriptListner):
-        scriptListner()
-
-    from ExecutingScript import *
-    ExecutingScript.init('initNoThrow_PyUnit')
-
-except Exception as ex:
-    _Exception = ex
-
-import unittest
-
-class Test_initNoThrow(unittest.TestCase):
-    def test(self):
-        if _Exception is not None:
-            raise _Exception
-
+# This test should only be run as an executable.
 if __name__ == '__main__':
+
+    _Exception = None
+    try:
+        def scriptBroadcaster(scriptListner):
+            scriptListner()
+
+        from ExecutingScript import *
+        ExecutingScript.init('initNoThrow_PyUnit')
+
+    except Exception as ex:
+        _Exception = ex
+
+    import unittest
+
+    class Test_initNoThrow(unittest.TestCase):
+        def test(self):
+            if _Exception is not None:
+                raise _Exception
+
     import sys
     unittest.main(argv=[sys.argv[0]])
--- util/tests_ExecutingScript/initThrowDueToMissingScriptBroadcaster_PyUnit.py
+++ util/tests_ExecutingScript/initThrowDueToMissingScriptBroadcaster_PyUnit.py
@@ -1,26 +1,27 @@
 #! /usr/bin/env python3.2
 
-_Exception = None
-try:
-    from ExecutingScript import *
-    ExecutingScript.init('initNoThrowDueToMissingScriptBroadcaster')
-
-except Exception as ex:
-    _Exception = ex
-
-import unittest
-
-class Test_initThrowDueToMissingScriptBroadcaster(unittest.TestCase):
-    def test(self):
-        expectedErrMsgSubStr = \
-          '__main__ module missing the following attributes: scriptBroadcaster'
-        with self.assertRaisesRegex(
-          ExecutingScript.RequirementUnsatisfiedError,
-          expectedErrMsgSubStr):
-            if(_Exception is not None):
-                raise _Exception
-
+# This test should only be run as an executable.
 if __name__ == '__main__':
-    import sys
 
+    _Exception = None
+    try:
+        from ExecutingScript import *
+        ExecutingScript.init('initNoThrowDueToMissingScriptBroadcaster')
+
+    except Exception as ex:
+        _Exception = ex
+
+    import unittest
+
+    class Test_initThrowDueToMissingScriptBroadcaster(unittest.TestCase):
+        def test(self):
+            expectedErrMsgSubStr = \
+            '__main__ module missing the following attributes: scriptBroadcaster'
+            with self.assertRaisesRegex(
+            ExecutingScript.RequirementUnsatisfiedError,
+            expectedErrMsgSubStr):
+                if(_Exception is not None):
+                    raise _Exception
+
+    import sys
     unittest.main(argv=[ sys.argv[0] ])
