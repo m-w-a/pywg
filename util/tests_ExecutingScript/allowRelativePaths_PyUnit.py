237b18f5258700faeae9516197abfd1e70491f6e
Mostafa <mostafa_working_away@yahoo.com>
2012-08-27 23:33:44 -0700

    Add more tests.

---
+++ util/tests_ExecutingScript/allowRelativePaths_PyUnit.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3.2
+
+from ExecutingScript import *
+ExecutingScript.allowRelativePaths('..')
+
+from .DummyModuleForImportTest_1 import *
+
+import unittest
+
+class Test_allowRelativePaths(unittest.TestCase):
+    def test(self):
+        pass
+
+if __name__ == '__main__':
+    import sys
+    unittest.main(argv=[sys.argv[0]])
