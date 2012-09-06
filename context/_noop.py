cebd80e457f0a63c2b6f6ac95170b71695445998
Mostafa <mostafa_working_away@yahoo.com>
2012-09-06 13:20:15 -0700

    Document.

--- context/_noop.py
+++ context/_noop.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3.2
 
 class Noop:
+    """
+    Context manager class which does nothing on __enter__ and __exit__.
+    """
     def __enter__(self):
         pass
     def __exit__(self, *exc_info):
@@ -9,4 +12,7 @@ class Noop:
 _Noop = Noop()
 
 def noop() -> Noop:
+    """
+    Returns a singleton instance of Noop context manager.
+    """
     return _Noop
