57ad5fb9eb1583665d4c8b0815f8853b2341dfec
Mostafa <mostafa_working_away@yahoo.com>
2012-09-09 18:35:02 -0700

    Fix.

--- lang/__init__.py
+++ lang/__init__.py
@@ -1,10 +1,8 @@
-from ._addStaticMarkerIfNotPresent import *
 from ._Enum import *
 from ._StaticInitor import *
 
 __all__ = \
   [
-    'addStaticMarkerIfNotPresent',
     'Enum',
     'StaticInitor'
   ]
